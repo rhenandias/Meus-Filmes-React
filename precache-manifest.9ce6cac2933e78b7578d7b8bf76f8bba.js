ac98ba5417d5d0fd2f1a6027e6d443aa63e60717
Rhenan Dias <diasrhenan@gmail.com>
2020-05-23 20:20:43 -0300

    Updates

---
+++ precache-manifest.9ce6cac2933e78b7578d7b8bf76f8bba.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b7fa633875ba1f8949ea856131cad7e",
+    "url": "/meus-filmes-react/index.html"
+  },
+  {
+    "revision": "dbfd7d1229ac34fc2191",
+    "url": "/meus-filmes-react/static/css/main.0868177c.chunk.css"
+  },
+  {
+    "revision": "46c9939b893dc3cde776",
+    "url": "/meus-filmes-react/static/js/2.56e06ab4.chunk.js"
+  },
+  {
+    "revision": "dbfd7d1229ac34fc2191",
+    "url": "/meus-filmes-react/static/js/main.60c0e391.chunk.js"
+  },
+  {
+    "revision": "3a002b9f29e67dbf1645",
+    "url": "/meus-filmes-react/static/js/runtime-main.6a39c741.js"
+  },
+  {
+    "revision": "b2138751ec3c40bb5c24fcea41aaff9d",
+    "url": "/meus-filmes-react/static/media/error.b2138751.svg"
+  },
+  {
+    "revision": "3aea435ed24c84900442dd97e174f246",
+    "url": "/meus-filmes-react/static/media/info.3aea435e.svg"
+  },
+  {
+    "revision": "9a40f046d04936e649a60cf7b5b780e3",
+    "url": "/meus-filmes-react/static/media/loading.9a40f046.svg"
+  },
+  {
+    "revision": "311313cded0dc23bba479b786ac0aad8",
+    "url": "/meus-filmes-react/static/media/plus.311313cd.svg"
+  },
+  {
+    "revision": "1a7d5c77da525d37b24e811b85516378",
+    "url": "/meus-filmes-react/static/media/search1.1a7d5c77.svg"
+  },
+  {
+    "revision": "b3e137fcdd64784ee34420ac34608148",
+    "url": "/meus-filmes-react/static/media/search2.b3e137fc.svg"
+  },
+  {
+    "revision": "d0b3cb69bbf545371424c1e3e1ebfc34",
+    "url": "/meus-filmes-react/static/media/video-player.d0b3cb69.svg"
+  },
+  {
+    "revision": "f31bfd8cec34977907864845a6b83eaa",
+    "url": "/meus-filmes-react/static/media/warning.f31bfd8c.svg"
+  }
+]);
